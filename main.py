8eb5984aaef2eac2e5c7af0f15889c7039e13929
BowGaming <109923007+BowGaming@users.noreply.github.com>
2026-06-27 23:16:37 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads variables from .env
+TOKEN = os.getenv("DISCORD_TOKEN")
+BOT_PREFIX = os.getenv("BOT_PREFIX", "!")  # fallback to "!" if not set
+
+
+class Amalgam(commands.Bot):
+    async def setup_hook(self):
+        initial_extensions = [
+          #add cogs here like this:
+          #"cogs.example"
+        ]
+
+        for extension in initial_extensions:
+            await self.load_extension(extension)
+
+
+intents = discord.Intents.all()
+
+get_pre = lambda bot, message: BOT_PREFIX
+
+bot = Amalgam(
+    command_prefix=get_pre, intents=intents, max_messages=16
+)
+
+@bot.event
+async def on_connect():
+    print("Loaded Discord")
+
+@bot.event
+async def on_ready():
+    print("------")
+    print("Logged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    print(discord.utils.utcnow().strftime("%d/%m/%Y %I:%M:%S:%f"))
+    print("------")
+
+@bot.check
+async def globally_block_dms(ctx):
+    return ctx.guild is not None
+
+bot.run(TOKEN, reconnect=True)
