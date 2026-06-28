cfe5067f7968ea8bce50f41a8bdad1b7f4bd8b5a
BowGaming <109923007+BowGaming@users.noreply.github.com>
2026-06-28 21:35:10 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+BOT_PREFIX = os.getenv("BOT_PREFIX", "~")
+
+guild_MD = int(os.getenv("GUILD_MD", 0))
+guild_DCO = int(os.getenv("GUILD_DCO", 0))
+comic_review_channel_MD = int(os.getenv("COMIC_REVIEW_CHANNEL_MD", 0))
+comic_review_channel_DCO = int(os.getenv("COMIC_REVIEW_CHANNEL_DCO", 0))
+review_reaction_emoji_MD = int(os.getenv("REVIEW_REACTION_EMOJI_MD", 0))
+review_reaction_emoji_DCO = int(os.getenv("REVIEW_REACTION_EMOJI_MD", 0))
