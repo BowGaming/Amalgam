1ac48b379d40007fa2da61d364878b725f6aabac
BowGaming <109923007+BowGaming@users.noreply.github.com>
2026-06-29 22:09:35 +0200

    Update review.py

--- cogs/review.py
+++ cogs/review.py
@@ -193,7 +193,7 @@ class ReviewCog(commands.Cog) :
 
         await self.forward_review(message, out_review_channel_id)
         
-        # Remove previous embed messages from bot to keep latest at bottom
+        # Remove previous embed messages from bot to keep latest at bottom in out server
         out_channel = self.bot.get_channel(out_review_channel_id)
         async for msg in out_channel.history(limit=5):
             if msg.author == self.bot.user:
@@ -203,6 +203,10 @@ class ReviewCog(commands.Cog) :
                     embed = msg.embeds[0]
                     if embed.title == self.review_instruction_embed.title:
                         await msg.delete()    
+
+        # Send sticky embeds at bottom in out server
+        await out_channel.send(embed=self.review_instruction_embed)
+        await out_channel.send(content=self.format_message)
             
 async def setup(bot: commands.Bot) :
     await bot.add_cog(ReviewCog(bot))
